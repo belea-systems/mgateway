397540ff81aa96faab0dcc2dd4006d02a7cd7b03
bizzare_owl <yummypotatopie@yandex.ru>
2026-08-06 14:14:57 +0300

    feat: specification implementatinon

--- src/main/java/ru/gitverse/bizzareowl/mgateway/web/MessageControllerImpl.java
+++ src/main/java/ru/gitverse/bizzareowl/mgateway/web/MessageControllerImpl.java
@@ -5,15 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gitverse.bizzareowl.mgateway.gateway.AlertMessageHandler;
 import ru.gitverse.bizzareowl.mgateway.gateway.ProcessedAlertMessage;
 import ru.gitverse.bizzareowl.mgateway.gateway.RawAlertMessage;
 import ru.gitverse.bizzareowl.mgateway.persistence.DeduplicationRepository;
+import ru.gitverse.bizzareowl.mgateway.persistence.entities.MessageDeduplicationRecord;
 import ru.gitverse.bizzareowl.mgateway.persistence.entities.MessageDeduplicationRecordPrimaryKey;
 import ru.gitverse.bizzareowl.mgateway.web.dto.MessageSavedResponseDto;
 import ru.gitverse.bizzareowl.mgateway.web.dto.RawAlertMessageDto;
@@ -31,14 +32,20 @@ public class MessageControllerImpl implements MessageController {
     private final RawAlertMassageDtoMapper rawAlertMassageDtoMapper = Mappers.getMapper(RawAlertMassageDtoMapper.class);
 
     @PostMapping
+    @Transactional
     @Override
     public ResponseEntity<MessageSavedResponseDto> sendMessage(@Valid @RequestBody RawAlertMessageDto rawAlertMessageDto) {
         final RawAlertMessage rawAlertMessage = rawAlertMassageDtoMapper.toDomain(rawAlertMessageDto);
-        if (deduplicationRepository.existsById(new MessageDeduplicationRecordPrimaryKey(
-                rawAlertMessage.messageId(), rawAlertMessage.messageData().id(), rawAlertMessage.messageData().source()))
-        ) {
+        final MessageDeduplicationRecordPrimaryKey key = new MessageDeduplicationRecordPrimaryKey(
+                rawAlertMessage.messageId(), rawAlertMessage.messageData().id(), rawAlertMessage.messageData().source());
+
+        if (deduplicationRepository.existsById(key)) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
+
+        final MessageDeduplicationRecord messageDeduplicationRecord = new MessageDeduplicationRecord();
+        messageDeduplicationRecord.setKey(key);
+        deduplicationRepository.save(messageDeduplicationRecord);
 
         final ProcessedAlertMessage processedAlertMessage = alertMessageHandler.handle(rawAlertMassageDtoMapper.toDomain(rawAlertMessageDto));
         return ResponseEntity
